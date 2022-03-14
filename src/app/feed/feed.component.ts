a34c4edac7ee66f5acb4c376c4715533eba642bc
Azt3kCode <alexis.aets18@gmail.com>
2022-03-14 12:00:12 -0600

    Vista feed

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-feed',
@@ -18,27 +19,44 @@ export class FeedComponent implements OnInit {
   postComment(comentario: string, publicacion: any): void {
     publicacion.comentario = this.comentario;
     this.comentario = "";
+    swal("Enviado", "Comentario realizado correctamente", "success");
   }
 
+  usuario = {
+    fotoPerfil: "https://media1.tenor.com/images/c0dbc6c2c624e781eb48d255ffca9a7f/tenor.gif?itemid=13016571",
+    nombre: "Azt3kCode"
+  }
+  
   publicaciones = [ {
-    "usuario": "@max",
-    "imagen": "https://media.tenor.com/images/3fccf5eb3a083b64754997ddf0a8d3ce/tenor.gif",
-    "caption": "Aqui en Cancun...",
+    "usuario": "Angelspartan01",
+    "imagen": "https://i.pinimg.com/originals/f9/13/57/f9135788c6aeeec438abb986f283936c.gif",
+    "caption": "Awesome!",
     "comentario": ""
   },
   {
-    "usuario": "@amsiedad",
-    "imagen": "../assets/imagenes/amsiedad.webp",
+    "usuario": "DavJT",
+    "imagen": "https://media1.tenor.com/images/728b1bf7d68319216cc3d889d8d4464a/tenor.gif?itemid=14799468",
     "caption": "Me da amsiedad...",
     "comentario": ""
   },
   {
-    "usuario": "@perrillo",
-    "imagen": "../assets/imagenes/meme-perrillo.jpeg",
+    "usuario": "Bazz",
+    "imagen": "https://th.bing.com/th/id/R.42455498933d57e8af7156813472d0fa?rik=ZY14wlbzBPkQ%2bA&pid=ImgRaw&r=0",
     "caption": "Unos perrillos :( ",
     "comentario": ""
   }
   ];
+
+  like = false;
+  bookmark = false;
+
+  likePost(): void {
+    this.like = !this.like;
+  }
+
+  bookmarkPost(): void {
+    this.bookmark = !this.bookmark;
+  }
 
 
 }
--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -5,11 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.css']
 })
+
 export class TabsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  seleccion_perfil = true;
+  seleccion_feed = false;
+
+  tab_location(pagina: string):void {
+    if (pagina == "perfil") {
+      this.seleccion_perfil = true;
+      this.seleccion_feed = false;
+    } else {
+      this.seleccion_perfil = false;
+      this.seleccion_feed = true;
+    }
   }
 
 }
