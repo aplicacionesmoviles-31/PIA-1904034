5dfd084db30a0d08490daa752041672454980003
Azt3kCode <alexis.aets18@gmail.com>
2022-03-15 01:10:25 -0600

    Modificacion de Routes Module

--- src/app/routes.module.ts
+++ src/app/routes.module.ts
@@ -5,11 +5,22 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import  { FeedComponent } from './feed/feed.component';
 import { PerfilComponent } from './perfil/perfil.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 const routes: Routes = [
-  { path: 'feed', component: FeedComponent },
-  { path: 'perfil', component: PerfilComponent }
-]
+  /*{ path: 'feed', component: FeedComponent },
+  { path: 'perfil', component: PerfilComponent }*/
+  { 
+    path: 'feed',
+    component: FeedComponent,
+    loadChildren: () => import('./feed/feed.module').then(m => m.FeedModule)
+  },
+  {
+    path: 'perfil',
+    component: PerfilComponent,
+    loadChildren: () => import('./perfil/perfil.module').then(m => m.PerfilModule)
+  }
+];
 
 @NgModule({
   declarations: [],
---
+++ src/app/tabs/tabs.module.ts
@@ -0,0 +1,17 @@
+import { IonicModule } from "@ionic/angular";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
+import {TabsComponent} from './tabs.component'
+
+@NgModule({
+    imports: [
+        IonicModule,
+        CommonModule,
+        FormsModule,
+
+    ],
+    declarations: [TabsComponent]
+})
+export class TabsPageModule {}
